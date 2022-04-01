4be94d5605536915741471c11e1aba9c5b1644f2
Odenir <odenirdev@gmail.com>
2022-04-01 11:18:17 -0300

    Ajuste: Componentização da sidebar e conteudos

---
+++ README.md
@@ -0,0 +1,15 @@
+# My movies
+
+> App de lista de filmes
+
+## Como rodar o projeto
+
+### Ambiente de desenvolvimento
+
+```bash
+yarn dev
+```
+
+## Referências
+
+Aplicação desenvolvido como desafio do curso Ignite da Rocketseat.
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,49 @@
-export function Content() {
-  // Complete aqui
+import React, { useEffect, useState } from "react";
+
+import { MovieCard } from "./MovieCard";
+
+import { MovieProps } from "../interfaces/movie";
+import { GenreResponseProps } from "../interfaces/genre";
+
+import { api } from "../services/api";
+
+interface ContentProps {
+  selectedGenreId: number;
+  selectedGenre: GenreResponseProps;
+}
+
+export function Content({ selectedGenreId, selectedGenre }: ContentProps) {
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  useEffect(() => {
+    api
+      .get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`)
+      .then((response) => {
+        setMovies(response.data);
+      });
+  }, [selectedGenreId]);
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span>{selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
